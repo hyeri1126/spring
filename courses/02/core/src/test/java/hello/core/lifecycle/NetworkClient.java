864bd0bb5a2dcd50f39de3e3058810ed2f17d75e
hyeri1126 <hyeri1126@ewhain.net>
2025-11-19 09:26:25 +0900

    test: 스프링 빈 생명주기 테스트 추가

---
+++ courses/02/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,44 @@
+package hello.core.lifecycle;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+public class NetworkClient  {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출" + url);
+    }
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    // 서비스 시작시 호출
+    @PostConstruct
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url + "message" + message);
+    }
+
+    // 서비스 종료시 호출
+    @PreDestroy
+    public void disconnect(){
+        System.out.println("disconnect " + url);
+    }
+
+    public void init(){
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close(){
+        disconnect();
+    }
+
+}

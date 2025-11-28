b58ad59b6cf906cdfe39c096991a803eda6fe95a
hyeri1126 <hyeri1126@ewhain.net>
2025-11-29 06:19:08 +0900

    feat: 상품 도메인 개발 - item 객체 및 레포지토리

---
+++ courses/03-스프링 mvc/item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,46 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    // 아이템 저장
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    // 아이템 조회
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    // 모든 아이템 조회
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    // 아이템 수정
+    public void update(Long itemId, Item updateParam) {
+        Item item = findById(itemId);
+        item.setItemName(updateParam.getItemName());
+        item.setPrice(updateParam.getPrice());
+        item.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+
+}
